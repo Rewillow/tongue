469876beaa4cb57a1aa66e2b84c7675498776533
Samuele <samuele.gugliotta23@gmail.com>
2024-03-07 16:08:34 +0100

    Aggiunto express-session al file principale

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require("express")
+const session = require("express-session")
 const cors = require("cors")
 const app = express()
 const userRoute = require("./routes/user.route")
