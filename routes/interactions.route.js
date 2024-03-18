ae1b40309b9700c210f8e5fc3b73d087dd38ea6e
Samuele <samuele.gugliotta23@gmail.com>
2024-03-18 16:28:20 +0100

    Miglioramento Naming delle rotte

--- routes/interactions.route.js
+++ routes/interactions.route.js
@@ -2,11 +2,11 @@ const express = require("express")
 const router = express()
 const interactionsController = require("../controllers/interactions.controller")
 
-router.post('/post/interaction/:id', interactionsController.createInteraction) // Rotta per la creazione di un'interazione
-router.delete('/post/interaction/:id', interactionsController.deleteAllInteractions) // Rotta per eliminare tutte le interazioni
-router.put('/post/interaction/:id', interactionsController.changeInteraction) // Rotta per cambiare un'interazione
-router.delete('/post/interaction/comment/:id', interactionsController.deleteComment) // Rotta per eliminare solo il commento
-router.delete('/post/interaction/like/:id', interactionsController.deleteLike) // Rotta per eliminare solo il mi piace
+router.post('/posts/interaction/:id', interactionsController.createInteraction) // Rotta per la creazione di un'interazione
+router.delete('/posts/interaction/:id', interactionsController.deleteAllInteractions) // Rotta per eliminare tutte le interazioni
+router.put('/posts/interaction/:id', interactionsController.changeInteraction) // Rotta per cambiare un'interazione
+router.delete('/posts/interaction/comment/:id', interactionsController.deleteComment) // Rotta per eliminare solo il commento
+router.delete('/posts/interaction/like/:id', interactionsController.deleteLike) // Rotta per eliminare solo il mi piace
 
 
 module.exports = router
--- routes/post.route.js
+++ routes/post.route.js
@@ -2,12 +2,12 @@ const express = require("express")
 const router = express.Router()
 const postController = require('../controllers/post.controller')
 
-router.get('/post', postController.filterInteractions) // Rotta che restituisce tutti i post coi rispettivi aggregati
-router.post('/post/:userId', postController.postCreate) // Rotta per la creazione di un post
-router.delete('/post/:id', postController.postDelete) // Rotta per eliminare un post
-router.put('/post/:id', postController.postChange) // Rotta per modificare un post
-router.get('/post/filterByCity', postController.filterByCity) // Rotta che restituisce i post filtrati per città e data
-router.get('/post/createdDate', postController.postFilter) // Rotta per restituire i post filtrati tramite data
+router.get('/posts', postController.filterInteractions) // Rotta che restituisce tutti i post coi rispettivi aggregati
+router.post('/posts/:userId', postController.postCreate) // Rotta per la creazione di un post
+router.delete('/posts/:id', postController.postDelete) // Rotta per eliminare un post
+router.put('/posts/:id', postController.postChange) // Rotta per modificare un post
+router.get('/posts/filterByCity', postController.filterByCity) // Rotta che restituisce i post filtrati per città e data
+router.get('/posts/createdDate', postController.postFilter) // Rotta per restituire i post filtrati tramite data
 
 
 module.exports = router
